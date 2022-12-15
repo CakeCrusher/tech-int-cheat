8d610830c62df6d406f3be5f2481d8c039b2c4eb
Sebastian Sosa <1sebastian1sosa1@gmail.com>
2022-12-15 15:55:25 -0500

    better popup UI

--- popup/popup.js
+++ popup/popup.js
@@ -2,8 +2,10 @@ console.log("Popup!!")
 // send a message to the service worker of type "RECORD"
 
 const startBtn = document.getElementById("start_rec_btn");
+const statusSpan = document.getElementById("status");
 // add event listener to the button
 startBtn.addEventListener("click", function () {
+  statusSpan.innerText = "Recording...";
   chrome.runtime.sendMessage({ type: "RECORD" });
 })
 
@@ -11,6 +13,7 @@ startBtn.addEventListener("click", function () {
 const btn = document.getElementById("btn");
 // add event listener to the button
 btn.addEventListener("click", function () {
+  statusSpan.innerText = "Awaiting results...";
   chrome.runtime.sendMessage({ type: "STOP_RECORDING" });
 })
 
@@ -18,6 +21,7 @@ btn.addEventListener("click", function () {
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.type === "SUGGESTED_CODE") {
     // get element of id "code"
+    statusSpan.innerText = "Idle";
     const code = document.getElementById("code");
     // set the innerHTML of the code element to the code
     code.innerText = request.code;
